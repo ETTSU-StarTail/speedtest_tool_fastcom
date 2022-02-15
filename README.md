d0bef32cde695f2877299a53d659a7c7425e0489
ETTSU-StarTail <46245666+ETTSU-StarTail@users.noreply.github.com>
2022-02-15 18:04:54 +0900

    実装準備、次はI/Oだけでも設計する。

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # speedtest_tool-fastcom
- 
+
+speedtest_tool の fast.com 版
+
+pyppeteer を使用して Fast.com を利用することでネットワーク速度計測をする。
---
+++ speedtest_tool-fastcom/main.py
@@ -0,0 +1,8 @@
+def main() -> None:
+    print("Start Program")
+
+    print("End Program")
+
+
+if __name__ == "__main__":
+    main()
